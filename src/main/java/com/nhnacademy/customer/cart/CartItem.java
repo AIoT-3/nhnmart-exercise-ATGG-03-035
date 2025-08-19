30a0d390d27f4ef97aee734201a518b7373b3259
nhn-academy-marco <marco@ nhnacademy.com>
2025-08-19 11:25:08 +0900

    주석 수정 및 오탈자 정정

--- src/main/java/com/nhnacademy/customer/cart/CartItem.java
+++ src/main/java/com/nhnacademy/customer/cart/CartItem.java
@@ -23,27 +23,27 @@ public class CartItem implements Serializable {
     private final int quantity;
 
     public CartItem(long productId, int quantity) {
-        //productId < 0 or quantity<0 IllegalArgumentException이 발생 합니다.
+        //productId < 0 or quantity < 0이면 IllegalArgumentException이 발생합니다.
         if(productId<0 || quantity <0){
             throw new IllegalArgumentException("productId > 0");
         }
 
-        //productId, quantity 를 초기화 합니다.
+        //productId, quantity를 초기화합니다.
         this.productId = productId;
         this.quantity = quantity;
     }
 
     public long getProductId() {
-        //productId 반환 합니다.
+        //productId를 반환합니다.
         return productId;
     }
 
     public int getQuantity() {
-        //quantity를 반환 합니다.
+        //quantity를 반환합니다.
         return quantity;
     }
 
-    //(projectId, quantity)를 기준으로 객체 비교를 하기 위해서 equals() 구현
+    //(productId, quantity)를 기준으로 객체 비교를 하기 위해서 equals() 구현
 
     @Override
     public boolean equals(Object o) {
@@ -53,7 +53,7 @@ public class CartItem implements Serializable {
         return productId == cartItem.productId && quantity == cartItem.quantity;
     }
 
-    //(projectId, quantity)를 기준으로 hashCode() 구현
+    //(productId, quantity)를 기준으로 hashCode() 구현
     @Override
     public int hashCode() {
         return Objects.hash(productId, quantity);
--- src/main/java/com/nhnacademy/customer/generator/CustomerGenerator.java
+++ src/main/java/com/nhnacademy/customer/generator/CustomerGenerator.java
@@ -21,7 +21,7 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * 회원생성 후 대기열에 등록 합니다.
+ * 회원생성 후 대기열에 등록합니다.
  */
 @Slf4j
 public class CustomerGenerator implements Runnable {
@@ -36,10 +36,10 @@ public class CustomerGenerator implements Runnable {
     private final static int DEFAULT_MONEY=10_00000;
 
     public CustomerGenerator(EnteringQueue enteringQueue) {
-        //TODO#4-1 enteringQueue null 이면 'IllegalArgumentException' 발생하는지 검증 합니다.
+        //TODO#4-1 enteringQueue null이면 'IllegalArgumentException'이 발생하는지 검증합니다.
 
 
-        //TODO#4-2 enteringQueue, atomicId 를 0으로 초기화 합니다.
+        //TODO#4-2 enteringQueue, atomicId를 0으로 초기화합니다.
         this.enteringQueue = null;
         atomicId=null;
 
@@ -48,21 +48,21 @@ public class CustomerGenerator implements Runnable {
     @Override
     public void run() {
 
-        /*TODO#4-4 generate() method를 이용해서 customer를 생성하고 enteringQueue에 등록 합니다.
+        /*TODO#4-4 generate() method를 이용해서 customer를 생성하고 enteringQueue에 등록합니다.
             - while 조건을 수정하세요.
-            - 1초 간격으로 회원을 entringQueue의 대기열에 등록 합니다.
+            - 1초 간격으로 회원을 enteringQueue의 대기열에 등록합니다.
         */
         while (true){
-            //1초 간격으로 회원을 entringQueue의 대기열에 등록 합니다.
+            //1초 간격으로 회원을 enteringQueue의 대기열에 등록합니다.
         }
     }
 
     private Customer generate(){
 
-        /*TODO#4-3 Customer 객체를 생성 후 반환 합니다.
-            - customer->id 는 atomicId를 사용하여 구현
-            - 회원이름은 random으로 생성 됩니다.
-               - 회원이름 생성시 https://github.com/Devskiller/jfairy 이용해서 구현 합니다.
+                 /*TODO#4-3 Customer 객체를 생성 후 반환합니다.
+            - customer->id는 atomicId를 사용하여 구현
+            - 회원이름은 random으로 생성됩니다.
+               - 회원이름 생성 시 https://github.com/Devskiller/jfairy 이용해서 구현합니다.
          */
 
         Customer customer = null;
