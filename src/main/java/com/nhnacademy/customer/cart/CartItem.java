7c8c78dc634e5bcd0141d3251390deff426bbaad
nhn-academy-marco <marco@ nhnacademy.com>
2026-01-20 10:07:20 +0900

    step2 장바구니 및 CartItem 메서드 구현 및 테스트 케이스 제거

--- src/main/java/com/nhnacademy/customer/cart/CartItem.java
+++ src/main/java/com/nhnacademy/customer/cart/CartItem.java
@@ -18,44 +18,36 @@ import java.util.Objects;
 public class CartItem implements Serializable {
 
     // 제품 아이디
-    private final long productId;
+    private long productId;
     // 제품 수량
-    private final int quantity;
+    private int quantity;
 
     public CartItem(long productId, int quantity) {
         // TODO#2-6 productId < 0 또는 quantity < 0이면 IllegalArgumentException이 발생합니다.
-        if (productId < 0 || quantity < 0) {
-            throw new IllegalArgumentException("productId or quantity cannot be negative");
-        }
 
         // TODO#2-7 productId, quantity를 초기화합니다.
-        this.productId = productId;
-        this.quantity = quantity;
     }
 
     public long getProductId() {
         // TODO#2-8 productId를 반환합니다.
-        return productId;
+        return 0L;
     }
 
     public int getQuantity() {
         // TODO#2-9 quantity를 반환합니다.
-        return quantity;
+        return 0;
     }
 
     // TODO#2-10 (productId, quantity)를 기준으로 객체 비교를 하기 위해 equals()를 구현합니다.
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CartItem cartItem = (CartItem) o;
-        return productId == cartItem.productId && quantity == cartItem.quantity;
+        return false;
     }
 
     // TODO#2-11 (productId, quantity)를 기준으로 hashCode()를 구현합니다.
     @Override
     public int hashCode() {
-        return Objects.hash(productId, quantity);
+        return 0;
     }
 }
