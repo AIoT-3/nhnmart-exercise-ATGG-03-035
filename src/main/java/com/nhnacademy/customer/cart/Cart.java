f0104a722ddafb211a4195e925e11dc0ee336b3d
OiJs <oijs9663@gmail.com>
2026-01-21 09:22:29 +0900

    feat: Cart, CartItem 구현

--- src/main/java/com/nhnacademy/customer/cart/Cart.java
+++ src/main/java/com/nhnacademy/customer/cart/Cart.java
@@ -18,7 +18,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Cart implements Serializable {
 
     private List<CartItem> cartItems;
@@ -27,22 +29,29 @@ public class Cart implements Serializable {
         // TODO#2-1 장바구니 아이템을 담을 수 있는 cartItems를 초기화합니다. 장바구니에 아이템이 추가될 때 동기화 처리를 위해 Collections.synchronizedList를 이용해 cartItems를 초기화합니다.
         // 즉 한 번에 한 아이템만 추가될 수 있고, 중복해서 추가될 수 없습니다.
         // https://docs.oracle.com/javase/6/docs/api/java/util/Collections.html#synchronizedList(java.util.List)
-        cartItems = null;
+        cartItems = Collections.synchronizedList(new ArrayList<>());
     }
 
     public void tryAddItem(CartItem cartItem) throws ProductAlreadyExistsException {
         // TODO#2-2 장바구니에 아이템이 이미 존재한다면 ProductAlreadyExistsException 예외가 발생합니다.
-
+        if(cartItems.contains(cartItem)) {
+            throw new ProductAlreadyExistsException(cartItem.getProductId());
+        }
         // TODO#2-3 cartItem에 아이템을 추가하는 코드를 작성하세요.
+        cartItems.add(cartItem);
+        log.info("장바구니에 상품을 담았습니다. 상품ID: {}", cartItem.getProductId());
     }
 
     public void clear(){
         // TODO#2-4 장바구니 cartItems를 초기화합니다.
+        cartItems.clear();
+        log.info("장바구니 초기화");
     }
 
     public List<CartItem> getCartItems() {
         // TODO#2-5 장바구니 아이템을 반환합니다.
-        return null;
+        log.info("장바구니 상품 반환");
+        return cartItems;
     }
 
 }
