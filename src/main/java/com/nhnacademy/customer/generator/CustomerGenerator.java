fa56f948500a8fa751b90e02d012d0492d77b961
OiJs <oijs9663@gmail.com>
2026-01-21 10:24:49 +0900

    feat: CustomerGenerator 구현

--- src/main/java/com/nhnacademy/customer/generator/CustomerGenerator.java
+++ src/main/java/com/nhnacademy/customer/generator/CustomerGenerator.java
@@ -37,8 +37,13 @@ public class CustomerGenerator implements Runnable {
 
     public CustomerGenerator(EnteringQueue enteringQueue) {
         // TODO#4-1 enteringQueue null이면 'IllegalArgumentException'이 발생하는지 검증합니다.
+        if(Objects.isNull(enteringQueue)) {
+            throw new IllegalArgumentException();
+        }
 
         // TODO#4-2 enteringQueue, atomicId를 0으로 초기화합니다.
+        this.enteringQueue = enteringQueue;
+        this.atomicId = new AtomicLong(0);
 
     }
 
@@ -49,6 +54,17 @@ public class CustomerGenerator implements Runnable {
             - while 조건을 수정하세요.
             - 1초 간격으로 회원을 enteringQueue의 대기열에 등록합니다.
         */
+        while (!Thread.currentThread().isInterrupted()){
+            //1초 간격으로 회원을 enteringQueue의 대기열에 등록합니다.
+            try {
+                Customer customer = generate();
+                enteringQueue.addCustomer(customer);
+                log.info("customer 추가 : {}", customer);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     private Customer generate(){
@@ -58,7 +74,8 @@ public class CustomerGenerator implements Runnable {
             - 회원이름은 랜덤으로 생성됩니다.
                - 회원이름 생성 시 https://github.com/Devskiller/jfairy 이용해서 구현합니다.
          */
-
-        return null;
+        Fairy fairy = Fairy.create();
+        Person person = fairy.person();
+        return new Customer(atomicId.incrementAndGet(), person.getFullName(), DEFAULT_MONEY);
     }
 }
